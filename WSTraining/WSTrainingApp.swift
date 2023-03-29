d4072657a9a8bdb761ec7cdf1f9958dd55876bf5
Glebzzzik <94132712+Glebzzzik@users.noreply.github.com>
2023-03-29 11:36:44 +0300

    Initial Commit

---
+++ WSTraining/WSTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WSTrainingApp.swift
+//  WSTraining
+//
+//  Created by Глеб Голощапов on 29.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct WSTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
